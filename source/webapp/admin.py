62adc04faa035a5b70401c13959c301aafa42329
Ulukbek <minzdravkr@gmail.com>
2020-08-01 16:20:51 +0600

    Zaregistriroval models v adminke i dobavil zapisi

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Review
 
-# Register your models here.
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'author', 'email', 'text')
+    list_display_links = ('pk', 'author')
+    list_filter = ('author',)
+    search_fields = ('author',)
+
+
+admin.site.register(Review, ReviewAdmin)

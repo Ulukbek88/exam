f75f257a4a371f93d598e3c6978bd0f884aa9c23
Ulukbek <minzdravkr@gmail.com>
2020-08-01 15:27:00 +0600

    В модели изменил названия поля Автор на Почту

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -11,7 +11,7 @@ STATUS_CHOICES = [
 
 class Review(models.Model):
     author = models.CharField(max_length=40, null=False, blank=False, default='Unknown', verbose_name='Автор')
-    email = models.EmailField(max_length=40, null=False, blank=False, default='Unknown', verbose_name='Автор')
+    email = models.EmailField(max_length=40, null=False, blank=False, default='Unknown', verbose_name='Почта')
     text = models.TextField(max_length=3000, null=False, blank=False, verbose_name='Текст')
     category = models.CharField(max_length=20, default=DEFAULT_CATEGORY, choices=CATEGORY_CHOICES, verbose_name='Статус')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')

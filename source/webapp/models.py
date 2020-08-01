c715f86d13dfc95495aedb0019bf27c98eb54a49
Ulukbek <minzdravkr@gmail.com>
2020-08-01 15:35:16 +0600

    Ispravil oshibki v models

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -13,7 +13,7 @@ class Review(models.Model):
     author = models.CharField(max_length=40, null=False, blank=False, default='Unknown', verbose_name='Автор')
     email = models.EmailField(max_length=40, null=False, blank=False, default='Unknown', verbose_name='Почта')
     text = models.TextField(max_length=3000, null=False, blank=False, verbose_name='Текст')
-    category = models.CharField(max_length=20, default=DEFAULT_CATEGORY, choices=CATEGORY_CHOICES, verbose_name='Статус')
+    status = models.CharField(max_length=20, default=DEFAULT_CATEGORY, choices=STATUS_CHOICES, verbose_name='Статус')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
 

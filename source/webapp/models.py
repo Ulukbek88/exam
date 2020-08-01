8079d5e224c3e987f62428cc2dc0aa19f2ae6924
Ulukbek <minzdravkr@gmail.com>
2020-08-01 15:25:47 +0600

    Добавлена модель

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+DEFAULT_CATEGORY = 'active'
+
+STATUS_CHOICES = [
+    ('active', 'Активно'),
+    ('blocked', 'Заблокировано'),
+]
+
+
+class Review(models.Model):
+    author = models.CharField(max_length=40, null=False, blank=False, default='Unknown', verbose_name='Автор')
+    email = models.EmailField(max_length=40, null=False, blank=False, default='Unknown', verbose_name='Автор')
+    text = models.TextField(max_length=3000, null=False, blank=False, verbose_name='Текст')
+    category = models.CharField(max_length=20, default=DEFAULT_CATEGORY, choices=CATEGORY_CHOICES, verbose_name='Статус')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
+
